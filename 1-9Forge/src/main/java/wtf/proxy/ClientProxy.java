23464a3adafd777f0ce763dba6b92a7f030c856c
elix-x <Elix-x@users.noreply.github.com>
2016-12-27 12:14:36 +0100

    Enable RP by default
    RP is enabled by default.
    
    There are 2 options - either it can be disabled by user or not. Please
    choose one and remove unnecessary code.

--- 1-9Forge/src/main/java/wtf/proxy/ClientProxy.java
+++ 1-9Forge/src/main/java/wtf/proxy/ClientProxy.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.IResourcePack;
+import net.minecraft.client.resources.ResourcePackRepository;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -22,7 +23,23 @@ public class ClientProxy extends CommonProxy {
 		
 	@Override
 	public void enableBlockstateTexturePack(){
-		//ReflectionHelper.<List<IResourcePack>>getPrivateValue(Minecraft.class, Minecraft.getMinecraft(), "defaultResourcePacks", "field_110449_ao").add(wtfResourcePack);
+		ResourcePackRepository repo = Minecraft.getMinecraft().getResourcePackRepository();
+		List<ResourcePackRepository.Entry> rpEnabled = repo.getRepositoryEntries();
+		ResourcePackRepository.Entry wtfResourcePack = null;
+		for(ResourcePackRepository.Entry rp : repo.getRepositoryEntriesAll()){
+			if(rp.getResourcePackName().equals("WTFOres")){
+				wtfResourcePack = rp;
+				break;
+			}
+		}
+		// Choose which one you want and remove the other lines.
+		
+		// 2 Lines below enable resource pack, but it stays visible in RPs GUI and can be disabled by user.
+		rpEnabled.add(wtfResourcePack);
+		repo.setRepositories(rpEnabled);
+
+		// A line below enables resource packs and hides it from RPs GUI making user unable to disable it.
+		ReflectionHelper.<List<IResourcePack>, Minecraft>getPrivateValue(Minecraft.class, Minecraft.getMinecraft(), "defaultResourcePacks", "field_110449_ao").add(wtfResourcePack.getResourcePack());
 	}
 	
 	@Override
